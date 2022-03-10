67c782339bccd677873ebe70f5281aad744e8481
mzgalleguillos <99368877+mzgalleguillos@users.noreply.github.com>
2022-03-09 19:16:15 -0500

    Initial commit

---
+++ Movimiento.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+   public GameObject characterPrincipal;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += Vector3.forward * Time.deltaTime;
+    }
+}
